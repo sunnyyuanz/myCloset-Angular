079d397183b1a29a4d54cfc853b131c830ada665
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2022-12-09 13:28:18 -0500

    login form

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,76 +1,96 @@
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {map} from 'rxjs/operators'
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://szmyflix.herokuapp.com/'
+const apiUrl = 'https://szmyflix.herokuapp.com/';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FetchApiDataService {
-//Inject the HttpClient module to the constructor params
-//This will provide HttpClient to the entries class, making it available via this.http
-  constructor(private http:HttpClient) {}
-//making the api call for the user registration endpoint
-public userRegistration(userDetails:any):Observable<any>{
-  console.log(userDetails);
-  return this.http.post(apiUrl+'users',userDetails).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  //Inject the HttpClient module to the constructor params
+  //This will provide HttpClient to the entries class, making it available via this.http
+  constructor(private http: HttpClient) {}
+  //making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public userLogin(userLoginInfo:any):Observable<any>{
-  console.log(userLoginInfo);
-  return this.http.post(apiUrl+'login',userLoginInfo).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public userLogin(userLoginInfo: any): Observable<any> {
+    console.log(userLoginInfo);
+    return this.http
+      .post(apiUrl + 'login', userLoginInfo)
+      .pipe(catchError(this.handleLoginError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public userGetInfo(userGetInfo:any):Observable<any>{
-  console.log(userGetInfo);
-  let user = localStorage.getItem('user')
-  return this.http.get(apiUrl+`users/${user}`,userGetInfo).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public userGetInfo(userGetInfo: any): Observable<any> {
+    console.log(userGetInfo);
+    let user = localStorage.getItem('user');
+    return this.http
+      .get(apiUrl + `users/${user}`, userGetInfo)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public GetWishList(wishList:any):Observable<any>{
-  console.log(wishList);
-  let user = localStorage.getItem('user')
-  return this.http.get(apiUrl+`users/${user}/collections`,wishList).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public GetWishList(wishList: any): Observable<any> {
+    console.log(wishList);
+    let user = localStorage.getItem('user');
+    return this.http
+      .get(apiUrl + `users/${user}/collections`, wishList)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public AddToWishList(product_id:any,wishList:any):Observable<any>{
-  console.log(product_id,wishList);
-  let user = localStorage.getItem('user')
-  return this.http.post(apiUrl+`users/${user}/collections/${product_id}`,wishList).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public AddToWishList(product_id: any, wishList: any): Observable<any> {
+    console.log(product_id, wishList);
+    let user = localStorage.getItem('user');
+    return this.http
+      .post(apiUrl + `users/${user}/collections/${product_id}`, wishList)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public RemoveFromWishList(product_id:any,wishList:any):Observable<any>{
-  console.log(product_id,wishList);
-  let user = localStorage.getItem('user')
-  return this.http.delete(apiUrl+`users/${user}/collections/${product_id}`,wishList).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public RemoveFromWishList(product_id: any, wishList: any): Observable<any> {
+    console.log(product_id, wishList);
+    let user = localStorage.getItem('user');
+    return this.http
+      .delete(apiUrl + `users/${user}/collections/${product_id}`, wishList)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public RemoveUser(userDetails:any):Observable<any>{
-  console.log(userDetails);
-  return this.http.delete(apiUrl+`users/${userDetails.username}`,userDetails).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public RemoveUser(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .delete(apiUrl + `users/${userDetails.username}`, userDetails)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-public EditUser(userDetails:any):Observable<any>{
-  console.log(userDetails);
-  return this.http.put(apiUrl+`users/${userDetails.username}`,userDetails).pipe(catchError(this.handleError))// pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function. 
-}
+  public EditUser(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .put(apiUrl + `users/${userDetails.username}`, userDetails)
+      .pipe(catchError(this.handleError)); // pipe() fucntion is from RxJS, a reactive programming library for Javascript, is used to combine multiple functions into a single function.
+  }
 
-private handleError(error:HttpErrorResponse):any{
-  if(error.error instanceof ErrorEvent){
-    console.error('Some error occurred:', error.error.message);
-  }else{
-    console.error(
-      `Error Status code ${error.status}`+
-      `Error body is: ${error.error}`
-    )
-    return throwError(
-      'Something bad happened; please try again later.'
-    )
+  private handleLoginError(error: HttpErrorResponse): any {
+    alert('Username or password is not correct');
+    return throwError('username or password is not correct');
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}` + `Error body is: ${error.error}`
+      );
+      return throwError('Something bad happened; please try again later.');
+    }
   }
 }
-}
-
-    
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -7,25 +7,26 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-user-registration-form',
-  templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss'],
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
 })
-export class UserRegistrationFormComponent implements OnInit {
-  @Input() userData = { Username: '', Password: '', ConfirmPassword: '' };
+export class UserLoginFormComponent implements OnInit {
+  @Input() userLoginData = { Username: '', Password: '' };
   constructor(
     public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar
   ) {}
 
+  //ngOnInit method is called once the component has received all its inputs (all its data-bound properties) from the calling component—in other words, the real-life user.
   ngOnInit(): void {}
 
   //This is the function responsible for sending the form inputs to the backend
-  registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe(
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userLoginData).subscribe(
       (result) => {
-        //logic for a successful user registration goes here!(To be implemented)
+        //logic for a successful user login goes here!(To be implemented)
         this.dialogRef.close(); //This will close the modal on success!
         console.log(result);
         this.snackBar.open(result, 'OK', {
